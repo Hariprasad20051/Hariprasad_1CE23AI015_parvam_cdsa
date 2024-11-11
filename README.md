2dea75b29a447fe77f18d2f9834fe17da2f91546
Hariprasad20051 <hariprasad.balaji007@gmail.com>
2024-11-11 13:09:06 +0530

    Complete Codes pushed

--- README.md
+++
@@ -1,2 +0,0 @@
-# Hariprasad_1CE23AI015_parvam_cdsa
-problem solving for c and dsa
--- day3/switch1.c
+++ day3/switch1.c
@@ -1,5 +1,75 @@
-#include<stdio.h>
-int main(){
-    prinf("123");
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+    char foodType = '\0';
+    int choice = 0;
+    puts("Welcome to Madurai Cafe");
+
+    do
+    {
+        puts("V:Veg N:Nonveg, Your choice plz: ");
+        foodType = tolower(getc(stdin));
+
+        // Clear the input buffer
+        while (getchar() != '\n');
+
+        switch (foodType)
+        {
+        case 'v':
+            puts("1:Banne-Dosa 2:Upma 3:Rava-idli \nYour choice? ");
+            scanf("%d", &choice);
+
+            switch (choice)
+            {
+            case 1:
+                puts("Your Tasty dosa Sir");
+                break;
+            case 2:
+                puts("Your yummy upma Maam");
+                break;
+            case 3:
+                puts("Your delicious idli Sir");
+                break;
+            default:
+                puts("We dont serve Grass Sir");
+                break;
+            }
+            break;
+
+        case 'n':
+            puts("1:Mutton-pulav 2:Chicken-chilly 3:Fish-fry. Your choice plz");
+            scanf("%d", &choice);
+
+            switch (choice)
+            {
+            case 1:
+                puts("Your Tasty Hanumantu");
+                break;
+            case 2:
+                puts("Guntur delicious Chicken Maam");
+                break;
+            case 3:
+                puts("Your spicy fish fry Maam");
+                break;
+            default:
+                puts("We dont serve Lizards Maam");
+                break;
+            }
+            break;
+
+        default:
+            puts("Invalid food type entered!");
+        }
+
+        puts("Do you wish to have more? 1:Yes 2:No");
+        scanf("%d", &choice);
+
+        // Clear the input buffer
+        while (getchar() != '\n');
+    } while (choice == 1);
+
+    puts("Thank you! Visit again");
     return 0;
 }
